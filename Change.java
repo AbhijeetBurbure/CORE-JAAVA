0112a1ad7f6d2040049cced1e7bb788b4598a2e0
AbhijeetBurbure <109050160+AbhijeetBurbure@users.noreply.github.com>
2022-07-14 16:26:34 +0530

    Add files via upload

---
+++ Change.java
@@ -0,0 +1,15 @@
+public class Change
+ {
+
+            public static void main(String[] args) 
+     {
+
+            double myDouble = 10.46;
+
+            int myInt = (int) myDouble;
+
+            System.out.println(myDouble);
+
+            System.out.println(myInt);
+     }
+}
