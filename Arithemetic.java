c593aba6f794753aff948eefd704a500bbd73730
AbhijeetBurbure <109050160+AbhijeetBurbure@users.noreply.github.com>
2022-07-15 17:55:44 +0530

    Add files via upload

---
+++ Arithemetic.java
@@ -0,0 +1,15 @@
+
+public class Arithemetic {
+	public static void main(String args[]){
+		
+		int a=35;
+		
+		int b=34;
+	
+		System.out.println("Sum of values is:" +  (a+b)); 
+		System.out.println("Substraction of values is:" + (a-b)); 
+		System.out.println("Multiplication of values is:" + (a*b));
+		System.out.println("Division of values is:" + (a/b)); 
+		}
+
+}
